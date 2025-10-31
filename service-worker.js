06f6471f34d2b1085af2d84346bda966ac404fd8
Isabelle19000 <gazelle19@gmail.com>
2025-10-31 21:40:46 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'suivi-alimentaire-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json'
+  // ajoute './icons/icon-192.png', './icons/icon-512.png' si présents
+];
+
+self.addEventListener('install', ev => {
+  ev.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', ev => {
+  ev.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', ev => {
+  ev.respondWith(
+    caches.match(ev.request).then(cached => cached || fetch(ev.request).catch(()=>caches.match('./')))
+  );
+});
